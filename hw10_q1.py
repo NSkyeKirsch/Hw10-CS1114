91c1f45d4d6639d3a5cde7c297da3771a7ec24b3
NSkyeKirsch <73858119+NSkyeKirsch@users.noreply.github.com>
2022-05-03 01:11:05 -0400

    finished q2 (minor date change)

--- hw10_q1.py
+++ hw10_q1.py
@@ -1,7 +1,7 @@
 """
 Author: Noa Kirschbaum
 Assignment / Part: HW10 - Q1
-Date due: 2022-04-28
+Date due: 2022-05-05
 I pledge that I have completed this assignment without
 collaborating with anyone else, in conformance with the
 NYU School of Engineering Policies and Procedures on
